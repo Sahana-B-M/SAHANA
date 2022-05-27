89df7bd47b1aca3e717b08e98f673e590e84912f
Sahana-B-M <100989312+Sahana-B-M@users.noreply.github.com>
2022-05-27 16:21:04 +0530

    Add files via upload

--- topo.c
+++ topo.c
@@ -1,48 +1,41 @@
 #include<stdio.h>
-int a[10][10],visited[10],n,count=0,topo[10];
-int k=0;
-void dfs(int v)
+int n,visited[10],count=0,k=0,top[10];
+int a[10][10];
+
+int dfs(int v)
 {
 	int w;
 	count++;
 	visited[v]=count;
 	for(w=0;w<n;w++)
-		if(visited[w]==0 && a[v][w]==1)
+		if(a[v][w]==1 && visited[w]==0)
 			dfs(w);
-	topo[k++]=v;
 	printf("%d\t",v);
+	top[k++]=v;
 }
 void main()
 {
-	int i,j;
-	printf("enter the number of vertices\n");
+	int v,i,j;
+	printf("Enter the no.of vertex:");
 	scanf("%d",&n);
-	printf("enter the matrix\n");
+	printf("Enter the adjacency matrix:\n");
 	for(i=0;i<n;i++)
-	{
 		for(j=0;j<n;j++)
 			scanf("%d",&a[i][j]);
-	}
-	printf("Adjacency matrix is \n");
+	printf("------Adjacency matrix------\n");
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
 			printf("%d\t",a[i][j]);
 		printf("\n");
 	}
-	printf("------DFS----------\n");
+	
 	for(i=0;i<n;i++)
-		visited[i]=0;
-	for(i=0;i<n;i++)
-		if(visited[i]==0)
-			dfs(i);
-	if(count==n)
-	{
-		printf("\ngraph is connected\n");
-	}else
-		printf("\ngraph is not connecting\n");
-	printf("\n Topological sorting is:\n");
+		visited[v]=0;
+	printf("\n----------DFS Traversal--------\n");
+	dfs(0);
+	
+	printf("\n----------Topological Sorting:--------\n");
 	for(i=n-1;i>=0;i--)
-		printf("%d\t",topo[i]);
-}
-
+		printf("%d\t",top[i]);
+}	
